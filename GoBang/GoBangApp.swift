f2a7bc11a527a1ee312c0b3f5c5bb8a5778ea866
Rambo <1434694372@qq.com>
2021-04-07 22:04:42 +0800

    Initial Commit

---
+++ GoBang/GoBangApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoBangApp.swift
+//  GoBang
+//
+//  Created by Rambo on 2021/4/7.
+//
+
+import SwiftUI
+
+@main
+struct GoBangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
